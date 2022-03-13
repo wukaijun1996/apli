78d99d1f7b6496311406bf37196442047c83ec9b
wukaijun1996 <1172917145@qq.com>
2022-03-14 00:31:42 +0800

    12

--- new/splider/1.py
+++ new/splider/1.py
@@ -3,20 +3,47 @@
 # @Author : wu
 # @File : 1.py 
 # @Software: PyCharm
+import time
 
-from urllib.request import urlopen
+from selenium.webdriver import Chrome
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
 
-url = "http://www.douban.com/"
-resp = urlopen(url)
 
-# print(resp.read().decode("utf-8"))
+web = Chrome()
+web.maximize_window()
+web.implicitly_wait(30)
+# web.get("https://ys.endata.cn/Information/Publish/WillSHow")
 
-html = resp.read().decode("utf-8")
-with open("shouye.html", 'w',encoding="utf-8") as fp:
+# sel_el = web.find_element(By.XPATH,'//*[@id="app"]/section/main/div/div[1]/div/section/div[1]/div[2]/div/div[1]/div[1]/div/div/input').click()
+#
+# web.find_element(By.XPATH,'/html/body/div/div[1]/div[1]/ul/li[2]').click()
+# # sel = Select(sel_el)
+# #
+# # for i in range(len(sel.options)):
+# #     sel.select_by_index(2)
+# # # //*[@id="pane-first"]/div[1]/div[2]
+#
+# xx = web.find_element(By.XPATH,'//*[@id="pane-first"]/div[1]/div[2]')
+# print(xx.text)
+# time.sleep(20)
 
-        fp.write(html,)
-print("爬取完毕")
+web.get('https://www.w3school.com.cn/tiy/t.asp?f=eg_html_select')
 
+web.find_element(By.XPATH,'//*[@id="tiy_btn_tryit"]/a').click()
+print("hiihi")
+web.find_element(By.XPATH,'/html/body/select')
+print(22)
+# sel_el = web.find_element(By.XPATH,'/html/body/select')
+#
+# sel = Select(sel_el)
+#
+# for i in range(len(sel.options)):
+#     sel.select_by_index(i)
+
+# /html/body/select
+
+time.sleep(2)
 
 
 
