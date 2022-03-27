7bf9b8b392a7c4d247dd934811363c9c4004a5d3
wukaijun1996 <1172917145@qq.com>
2022-03-27 22:01:09 +0800

    bootstrap

---
+++ 13_Blog/apps/models/__init__.py
@@ -0,0 +1,5 @@
+#-*- codeing = utf-8 -*-
+# @Time : 2022/3/26 20:56 
+# @Author : wu
+# @File : __init__.py.py 
+# @Software: PyCharm
---
+++ 13_Blog/settings.py
@@ -0,0 +1,21 @@
+#-*- codeing = utf-8 -*-
+# @Time : 2022/3/26 20:53 
+# @Author : wu
+# @File : settings.py 
+# @Software: PyCharm
+
+# 配置信息文件
+ENV = 'development'
+DEBUG = True
+# 数据库连接驱动
+SQLALCHEMY_DATASBASE_URI = 'mysql + pymysql://root:123456@127.0.0.1:3306/book_1'
+
+
+
+
+
+
+
+
+
+
